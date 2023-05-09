15fb71f56cbea77aa51b903a4eddd9bc49587ab1
hamzaougjjou <hamzaougjjou04@gmail.com>
2023-05-09 19:39:38 +0100

    add fire base to login page

--- src/components/NavPages.js
+++ src/components/NavPages.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
+import Login from '../auth/Login'
 import Home from './home/Home'
 import Posts from './posts/Posts'
 import Settings from './settings/Settings'
@@ -12,6 +13,7 @@ function NavPages() {
             <Route path='/settings' element={<Settings />} />
             <Route path='/posts' element={<Posts />} />
             <Route path='/trips' element={<Trips />} />
+            <Route path='/login' element={<Login />} />
         </Routes>
     )
 }
